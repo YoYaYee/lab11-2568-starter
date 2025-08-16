4713f102c062d632bc1b4efb3c1dd18d005690c3
YoYaYee <EDEN555007@gmail.com>
2025-08-16 14:51:17 +0700

    complete

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="chanadda" studentId="67062039" />
+          <Footer year="202" fullName="Pawornpruch Sanlee" studentId="670610715" />
         </div>
       </div>
     </div>
